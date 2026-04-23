6665309dd1d11837177e05ea2bd5b356ea3a9175
mpeck2023 <121977879+mpeck2023@users.noreply.github.com>
2026-04-22 21:12:56 -0400

    ascii fiddling

--- ascii/asciiImage.py
+++ ascii/asciiImage.py
@@ -13,6 +13,7 @@ def to_ascii(file, size = 150, directory = ""):
         image = image.convert("L")
         # image = image.point(lambda i: i/2 if(i > 85 and i < 170) else i)
 
+        # ImageEnhance.Contrast(image).enhance(1/4)
         image = ImageOps.fit(image, (size,size), centering=(0.5,0.0))
         image = image.reduce((1,2))
         image_px = image.get_flattened_data()
@@ -20,7 +21,7 @@ def to_ascii(file, size = 150, directory = ""):
 
     #@@BBRR**##$$PPXX00wwooIIccvv::++!!~~""..,,  " .-:=+##","$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"\^`'. "
     # dark_to_bright = "  .:--++****"
-    dark_to_bright = "  .:-+*$$#@"
+    dark_to_bright = "  ..::-++**$$#@"
     bright_to_dark = dark_to_bright[::-1]
     lines = [[dark_to_bright[int(image.getpixel((i,j)) / max(image_px) * (len(dark_to_bright) - 1))] for i in range(image.width)] for j in range(image.height)]
     lines = ["".join(line) for line in lines]
